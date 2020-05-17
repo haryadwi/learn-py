8dd9ed070b7b2c3b835a360705ab20bd1adaadff
haryadwi <haryadwi@gmail.com>
2020-05-17 21:58:00 +0700

    If Else

---
+++ LEARN/branch/ifelse.py
@@ -0,0 +1,8 @@
+# program ini menguji apakah suatu bilangan berjenis positif atau negatif
+
+bilangan = -5
+
+if bilangan >= 0:
+    print(bilangan, "Merupakan Bilangan Positif")
+else:
+    print(bilangan, "Merupakan Bilangan Negatif")
