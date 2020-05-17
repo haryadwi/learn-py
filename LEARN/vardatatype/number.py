f30c169e9f2fc14d41150562f7816fc59f23e7b6
haryadwi <haryadwi@gmail.com>
2020-05-17 13:37:18 +0700

    vardatatype

---
+++ LEARN/vardatatype/number.py
@@ -0,0 +1,6 @@
+x = 5
+print(x, "Tipenya adalah", type(x))
+x = 2.3
+print(x, "Tipenya adalah", type(x))
+x = 5+3j
+print(x, "Tipenya adalah", type(x))
---
+++ LEARN/vardatatype/string.py
@@ -0,0 +1,7 @@
+kalimat = "Halo Saya Jawir"
+
+print(kalimat)  # print string lengkap
+print(kalimat[0])  # print karakter pertama dalam kalimat
+print(kalimat[-1])  # print karakter terakhir dalam kalimat
+print(kalimat[2:8])  # print karakter indeks 2 - 8 dalam kalimat
+print(kalimat[:3])  # print karakter indeks 0 - 2 dalam kalimat
