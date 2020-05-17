f478a82a6dc525ae54027d17f7d779be9463e0cf
haryadwi <haryadwi@gmail.com>
2020-05-17 21:14:32 +0700

    Percabangan satu kondisi

---
+++ LEARN/branch/if.py
@@ -0,0 +1,10 @@
+# jika bilanga positif maka tampilkan pesan
+
+angka = 5
+if angka > 0:
+    print(angka, "Adalah Bilangan Positif")
+
+# inputan bernilai false maka tidak akan dieksekusi
+angka = -1
+if angka > 0:
+    print(angka, "Adalah Bilangan Positif")
