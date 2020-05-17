832a7cf75e63109b29a8c7875d64ddc844c68fe0
haryadwi <haryadwi@gmail.com>
2020-05-17 22:12:46 +0700

    ifelifelse

---
+++ LEARN/branch/ifelifelse.py
@@ -0,0 +1,10 @@
+# program penguji bilangan positif, nol, dan negatif
+
+bilangan = -4
+
+if bilangan > 0:
+    print(bilangan, "Merupakan Bilangan Positif")
+elif bilangan == 0:
+    print(bilangan, "Bernilai Nol")
+else:
+    print("Merupakan Bilangan Negatif")
