a822e1508bdffe89e1c228d390aa7fecef22fedd
haryadwi <haryadwi@gmail.com>
2020-05-17 20:16:52 +0700

    list pada python

---
+++ LEARN/vardatatype/list.py
@@ -0,0 +1,10 @@
+a = [5, 10, 15, 20, 25, 30, 35, 40]
+
+# menampilkan indeks ke 2 list yaitu angka 15
+print("a[2] =", a[2])
+
+# menampilkan indeks ke 0 sampai dengan ke 2
+print("a[0:3] =", a[0:3])
+
+# menampilkan indeks mulai dari ke 5
+print("a[:5] =", a[5:])
